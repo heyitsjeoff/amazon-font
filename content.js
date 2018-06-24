8f0bf424eeaf6c95196bf40ea120176431eda226
heyitsjeoff <heyitsjeoff@gmail.com>
2018-06-24 09:52:32 -0700

    add css node to override bookerly updated gitignore because mac

---
+++ content.js
@@ -0,0 +1,4 @@
+const style = document.createElement('style');
+style.type = 'text/css';
+style.appendChild(document.createTextNode('body {font-family: "Amazon Ember",Arial,sans-serif !important}'));
+document.documentElement.appendChild(style);
